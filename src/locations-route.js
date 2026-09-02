43c35f985e65d8d528b2a13342acb7773ff31b0f
N10ai <infon10miami@gmail.com>
2026-09-02 15:43:34 -0400

    Refresh locations module version

--- src/locations-route.js
+++ src/locations-route.js
@@ -1,2 +1,2 @@
-import './warehouse-locations.js?v=20260902-0530';
+import './warehouse-locations.js?v=20260902-1542';
 document.addEventListener('click',e=>{const b=e.target.closest?.('[data-subroute="warehouse_locations"],[data-subroute="settings_locations"]');if(!b)return;e.preventDefault();e.stopImmediatePropagation();window.nodaraSetActive?.(b.dataset.subroute);window.nodaraLocations?.()},{capture:true});
