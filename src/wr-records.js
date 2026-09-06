794a7e665137e47711932fb27fad115891a1dfa7
N10ai <infon10miami@gmail.com>
2026-09-06 13:25:22 -0400

    Cache-bust canonical WR records module

--- src/wr-records.js
+++ src/wr-records.js
@@ -1,2 +1,2 @@
-export { warehouseReceiptList, warehouseReceiptOpen as warehouseReceiptDetail } from './wr-records-unified.js?v=20260906-1300';
-import './wr-records-unified.js?v=20260906-1300';
+export { warehouseReceiptList, warehouseReceiptOpen as warehouseReceiptDetail } from './wr-records-unified.js?v=20260906-1320';
+import './wr-records-unified.js?v=20260906-1320';
