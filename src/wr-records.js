156930d7155758bdef1ac7fc218c307e8b1a3f67
N10ai <infon10miami@gmail.com>
2026-09-06 13:28:27 -0400

    Bump canonical WR records module

--- src/wr-records.js
+++ src/wr-records.js
@@ -1,2 +1,2 @@
-export { warehouseReceiptList, warehouseReceiptOpen as warehouseReceiptDetail } from './wr-records-unified.js?v=20260906-1320';
-import './wr-records-unified.js?v=20260906-1320';
+export { warehouseReceiptList, warehouseReceiptOpen as warehouseReceiptDetail } from './wr-records-unified.js?v=20260906-1330';
+import './wr-records-unified.js?v=20260906-1330';
