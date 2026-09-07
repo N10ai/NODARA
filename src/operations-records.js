ea446d3ca347af6e63614a862284827bbc4c6bee
N10ai <infon10miami@gmail.com>
2026-09-07 08:40:58 -0400

    Load stable operations save owner before enhancers

--- src/operations-records.js
+++ src/operations-records.js
@@ -1,2 +1,3 @@
 export { openShipments, openTransportOrders } from './operations-core.js?v=20260906-core4';
 import './operations-core.js?v=20260906-core4';
+import './operations-save-stable.js?v=20260907-core1';
