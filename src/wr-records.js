aee0353a2e90fb43732159b684a31ca39d4688ab
N10ai <infon10miami@gmail.com>
2026-09-07 22:18:11 -0400

    Bump native WR records workspace version

--- src/wr-records.js
+++ src/wr-records.js
@@ -1,2 +1,2 @@
-export { warehouseReceiptList, warehouseReceiptOpen as warehouseReceiptDetail } from './wr-records-unified.js?v=20260908-native1';
-import './wr-records-unified.js?v=20260908-native1';
+export { warehouseReceiptList, warehouseReceiptOpen as warehouseReceiptDetail } from './wr-records-unified.js?v=20260908-native2';
+import './wr-records-unified.js?v=20260908-native2';
