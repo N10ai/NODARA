eddc23c1c1ef496e200274bf79b165bd871e3769
N10ai <infon10miami@gmail.com>
2026-09-10 21:00:42 -0400

    Use single production WR records module

--- src/wr-records.js
+++ src/wr-records.js
@@ -1,2 +1,1 @@
-export { warehouseReceiptList, warehouseReceiptOpen as warehouseReceiptDetail } from './wr-records-unified.js?v=20260909-v11a';
-import './wr-records-unified.js?v=20260909-v11a';
+export { warehouseReceiptList, warehouseReceiptOpen as warehouseReceiptDetail } from './wr-records-unified.js?v=20260910-prod1';
