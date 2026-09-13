984d76426cb5e66cb5425841a64c36680d31684a
N10ai <infon10miami@gmail.com>
2026-09-13 07:23:09 -0400

    Load stabilized guided workflow runtime

--- src/transaction-ux-bootstrap.js
+++ src/transaction-ux-bootstrap.js
@@ -1,3 +1,3 @@
 if(!document.querySelector('link[data-transaction-ux]')){const l=document.createElement('link');l.rel='stylesheet';l.href='./transaction-ux.css?v=20260908-guided2';l.dataset.transactionUx='1';document.head.appendChild(l)}
-import './transaction-guidance-runtime.js?v=20260908-guided1';
+import './transaction-guidance-runtime.js?v=20260913-v35';
 import './field-voice-input.js?v=20260908-guided1';
