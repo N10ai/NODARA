faebac970659c25d003854d8973c844dcee4b5d9
N10ai <infon10miami@gmail.com>
2026-09-16 06:57:59 -0400

    Bump canonical shipment runtime after cargo stability fix

--- src/shipment-entry-v45.js
+++ src/shipment-entry-v45.js
@@ -1,3 +1,3 @@
-import './shipment-entry.js?v=20260916-v50';
-import './shipment-cohesion-v48.js?v=20260916-v50';
-import './shipment-stability-v50.js?v=20260916-v50';
+import './shipment-entry.js?v=20260916-v51';
+import './shipment-cohesion-v48.js?v=20260916-v51';
+import './shipment-stability-v50.js?v=20260916-v51';
