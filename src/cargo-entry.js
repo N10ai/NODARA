5f65ea99eff6feadfecae2d5edfd9df7e456fc41
N10ai <infon10miami@gmail.com>
2026-09-18 19:37:00 -0400

    Load direct cargo putaway editor

--- src/cargo-entry.js
+++ src/cargo-entry.js
@@ -2,7 +2,7 @@ import './cargo-workspace.js';
 import './transaction-cargo-workspaces.js';
 import './cargo-loader-smart-search.js';
 if(!document.querySelector('link[data-cargo-workspace-css]')){const l=document.createElement('link');l.rel='stylesheet';l.href='cargo-workspace.css?v=20260905-2215';l.dataset.cargoWorkspaceCss='1';document.head.appendChild(l)}
-import { createCargoExplorer } from './cargo-explorer.js';
+import { createCargoExplorer } from './cargo-explorer.js?v=20260918-putaway1';
 import { PACKAGE_TYPES } from './cargo-tree-builder.js';
 
 const main=document.getElementById('main');
