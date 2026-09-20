e717140a65a90ce27686da9ba24dbc8fac8a5640
N10ai <infon10miami@gmail.com>
2026-09-19 20:26:28 -0400

    Load dimensional location editor

--- src/locations-route.js
+++ src/locations-route.js
@@ -1,3 +1,3 @@
-import { openLocations } from './locations-core.js?v=20260906-core1';
+import { openLocations } from './locations-core.js?v=20260919-dims1';
 document.addEventListener('click',e=>{const b=e.target.closest?.('[data-subroute="warehouse_locations"],[data-subroute="settings_locations"]');if(!b)return;e.preventDefault();e.stopImmediatePropagation();window.nodaraSetActive?.(b.dataset.subroute);openLocations()},{capture:true});
 window.nodaraLocations=openLocations;
