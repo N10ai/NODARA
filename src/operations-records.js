5f4346319454153bd1340346ab6253fb80ea9222
N10ai <infon10miami@gmail.com>
2026-09-21 21:57:54 -0400

    Deploy Transport party address contact behavior

--- src/operations-records.js
+++ src/operations-records.js
@@ -1,3 +1,3 @@
-export { openShipments, openTransportOrders } from './operations-core.js?v=transport-step3-1790042161330';
-import './operations-core.js?v=transport-step3-1790042161330';
+export { openShipments, openTransportOrders } from './operations-core.js?v=transport-step4-1790042272453';
+import './operations-core.js?v=transport-step4-1790042272453';
 import './operations-save-stable.js?v=20260907-core1';
