60fb92c046aebbd3e9ab0cf0f6f9d64bd5ab0c86
N10ai <infon10miami@gmail.com>
2026-09-22 07:13:03 -0400

    Deploy isolated Operations core

--- src/operations-records.js
+++ src/operations-records.js
@@ -1,3 +1,3 @@
-export { openShipments, openTransportOrders } from './operations-core.js?v=transport-step4-1790042272453';
-import './operations-core.js?v=transport-step4-1790042272453';
+export { openShipments, openTransportOrders } from './operations-core.js?v=isolated-1790075582119';
+import './operations-core.js?v=isolated-1790075582119';
 import './operations-save-stable.js?v=20260907-core1';
