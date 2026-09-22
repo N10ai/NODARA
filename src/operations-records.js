1bae33eef965d28ec18cbd2d198b8f3262931ada
N10ai <infon10miami@gmail.com>
2026-09-21 21:56:05 -0400

    Deploy Transport shipper consignee fields

--- src/operations-records.js
+++ src/operations-records.js
@@ -1,3 +1,3 @@
-export { openShipments, openTransportOrders } from './operations-core.js?v=transport-step2-1790042108997';
-import './operations-core.js?v=transport-step2-1790042108997';
+export { openShipments, openTransportOrders } from './operations-core.js?v=transport-step3-1790042161330';
+import './operations-core.js?v=transport-step3-1790042161330';
 import './operations-save-stable.js?v=20260907-core1';
