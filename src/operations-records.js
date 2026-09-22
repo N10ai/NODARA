9443e7d42245103e2c7ab5f96afaa12c8c9abf45
N10ai <infon10miami@gmail.com>
2026-09-21 21:53:58 -0400

    Deploy isolated Transport customer search field

--- src/operations-records.js
+++ src/operations-records.js
@@ -1,3 +1,3 @@
-export { openShipments, openTransportOrders } from './operations-core.js?v=stable-1790040489854';
-import './operations-core.js?v=stable-1790040489854';
+export { openShipments, openTransportOrders } from './operations-core.js?v=transport-step1-1790042036223';
+import './operations-core.js?v=transport-step1-1790042036223';
 import './operations-save-stable.js?v=20260907-core1';
