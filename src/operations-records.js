59004e5ad1d744e951257fc1218753d80694e393
N10ai <infon10miami@gmail.com>
2026-09-21 21:55:12 -0400

    Deploy searchable Transport party fields

--- src/operations-records.js
+++ src/operations-records.js
@@ -1,3 +1,3 @@
-export { openShipments, openTransportOrders } from './operations-core.js?v=transport-step1-1790042036223';
-import './operations-core.js?v=transport-step1-1790042036223';
+export { openShipments, openTransportOrders } from './operations-core.js?v=transport-step2-1790042108997';
+import './operations-core.js?v=transport-step2-1790042108997';
 import './operations-save-stable.js?v=20260907-core1';
