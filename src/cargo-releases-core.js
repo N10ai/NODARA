6384cb62eed237c54ef1799b5489e0817fdc92d1
N10ai <infon10miami@gmail.com>
2026-09-25 08:26:41 -0400

    Deploy CR picker grouped search views

--- src/cargo-releases-core.js
+++ src/cargo-releases-core.js
@@ -1,3 +1,3 @@
-export { listCR, createCR, openCR } from './cargo-releases-core-v2.js?v=20260925-cr-search5';
-import { listCR, createCR, openCR } from './cargo-releases-core-v2.js?v=20260925-cr-search5';
+export { listCR, createCR, openCR } from './cargo-releases-core-v2.js?v=20260925-cr-group6';
+import { listCR, createCR, openCR } from './cargo-releases-core-v2.js?v=20260925-cr-group6';
 window.nodaraRelease=listCR;window.nodaraCRNew=createCR;window.nodaraCROpen=openCR;
