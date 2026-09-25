640f851d191b8ae614a44f79caa0ea76a07c459a
N10ai <infon10miami@gmail.com>
2026-09-25 08:22:47 -0400

    Deploy improved CR inventory search

--- src/cargo-releases-core.js
+++ src/cargo-releases-core.js
@@ -1,3 +1,3 @@
-export { listCR, createCR, openCR } from './cargo-releases-core-v2.js?v=20260925-cr-dense4';
-import { listCR, createCR, openCR } from './cargo-releases-core-v2.js?v=20260925-cr-dense4';
+export { listCR, createCR, openCR } from './cargo-releases-core-v2.js?v=20260925-cr-search5';
+import { listCR, createCR, openCR } from './cargo-releases-core-v2.js?v=20260925-cr-search5';
 window.nodaraRelease=listCR;window.nodaraCRNew=createCR;window.nodaraCROpen=openCR;
