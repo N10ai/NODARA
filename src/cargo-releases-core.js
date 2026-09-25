cee5399dccbee9117860947caaeb70592649e3c0
N10ai <infon10miami@gmail.com>
2026-09-25 08:17:01 -0400

    Deploy dense CR cargo interface

--- src/cargo-releases-core.js
+++ src/cargo-releases-core.js
@@ -1,3 +1,3 @@
-export { listCR, createCR, openCR } from './cargo-releases-core-v2.js?v=20260925-cr-availability3';
-import { listCR, createCR, openCR } from './cargo-releases-core-v2.js?v=20260925-cr-availability3';
+export { listCR, createCR, openCR } from './cargo-releases-core-v2.js?v=20260925-cr-dense4';
+import { listCR, createCR, openCR } from './cargo-releases-core-v2.js?v=20260925-cr-dense4';
 window.nodaraRelease=listCR;window.nodaraCRNew=createCR;window.nodaraCROpen=openCR;
