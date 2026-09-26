98b1c92f18cffe5063ecc6e0270341635ce0b2f1
N10ai <infon10miami@gmail.com>
2026-09-25 20:15:17 -0400

    Deploy Cargo Release lifecycle register

--- src/cargo-releases-core.js
+++ src/cargo-releases-core.js
@@ -1,3 +1,3 @@
-export { listCR, createCR, openCR } from './cargo-releases-core-v2.js?v=20260925-cr-group7';
-import { listCR, createCR, openCR } from './cargo-releases-core-v2.js?v=20260925-cr-group7';
+export { listCR, createCR, openCR } from './cargo-releases-core-v2.js?v=20260925-cr-register8';
+import { listCR, createCR, openCR } from './cargo-releases-core-v2.js?v=20260925-cr-register8';
 window.nodaraRelease=listCR;window.nodaraCRNew=createCR;window.nodaraCROpen=openCR;
