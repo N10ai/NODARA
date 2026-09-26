8c7fc20868de95b5adaecc87b22c49c6aab94a38
N10ai <infon10miami@gmail.com>
2026-09-25 21:22:53 -0400

    Deploy Transport Order lifecycle register

--- src/operations-records.js
+++ src/operations-records.js
@@ -1,3 +1,3 @@
-export { openShipments, openTransportOrders } from './operations-core.js?v=isolated-1790075582119';
-import './operations-core.js?v=isolated-1790075582119';
+export { openShipments, openTransportOrders } from './operations-core.js?v=transport-register-1790385900000';
+import './operations-core.js?v=transport-register-1790385900000';
 import './operations-save-stable.js?v=20260907-core1';
