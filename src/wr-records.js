1d260e0fdb99094dff66878019da3dd81e6df9d6
N10ai <infon10miami@gmail.com>
2026-09-25 20:10:41 -0400

    Deploy Warehouse Receipt lifecycle register

--- src/wr-records.js
+++ src/wr-records.js
@@ -1,1 +1,1 @@
-export { warehouseReceiptList, warehouseReceiptOpen as warehouseReceiptDetail } from './wr-records-unified.js?v=20260910-prod1';
+export { warehouseReceiptList, warehouseReceiptOpen as warehouseReceiptDetail } from './wr-records-unified.js?v=20260925-register1';
